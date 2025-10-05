de65b76bd072b5ee942c8ddf242de36dbc8fa516
Applelyyyy <islandgameming@gmail.com>
2025-10-05 14:57:22 +0700

    Run for Windows

--- run.bat
+++ run.bat
@@ -6,16 +6,16 @@ if not exist output (
 if not exist log (
     mkdir log
 )
-:: Compile...
+
 gcc main.c github_sync.c unit_test.c -o output/main.exe 2> log/Compile_error.txt
 
-:: Check if compilation succeeded
+
 if %errorlevel% neq 0 (
     echo ❌ Compilation failed. error in log Folder:
     type log\Compile_error.txt
     pause
     exit /b
 )
-echo "Compilation successful. Running program..."
+echo Compilation successful. Running program...
 start cmd /c "cd output && main.exe"
 exit
